d5f65d0a8c26fb9fa7aa24cbee3b87bab132cbe9
RizalAnas00 <belangsumerlang@gmail.com>
2026-03-27 13:20:34 +0700

    feat: Github Login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\GithubController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::controller(GithubController::class)->group(function(){
+    Route::get('auth/github', 'redirectToGithub')->name('auth.github');
+    Route::get('auth/github/callback', 'handleGithubCallback');
 });
